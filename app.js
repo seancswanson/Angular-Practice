4bcc34604c32d4f7fd7b206b6386fcc962b8307d
seanthaswan <seancswan@gmail.com>
2019-10-14 12:05:42 -0700

    Practicing more Angular directions, if/show/hide/class.

--- app.js
+++ app.js
@@ -12,22 +12,6 @@ myApp.controller('mainController', ['$scope', '$filter', '$timeout', function ($
         return $filter('lowercase')($scope.handle);
     };
 
-//    $scope.$watch('handle', function (newValue, oldValue) {
-//
-//        console.warn('Changed!');
-//        console.log('Old: ' + oldValue);
-//        console.log('New: ' + newValue);
-//
-//    });
 
-//    $timeout(function () {
-//
-//        //        $scope.$apply(function () {
-//        $scope.handle = 'newTwitterHandle';
-//        console.log('Scope changed!');
-//        //        });
-//
-//    }, 3000);
-
-
+    $scope.characters = 5;
 }]);
