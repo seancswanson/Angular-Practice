ab2e1a6c129a82283ec91d9d016dfb82db41055b
seanthaswan <seancswan@gmail.com>
2019-10-16 12:02:24 -0700

    Tidied up styling, experimenting with ng-repeat directive to make a list out of a Array of Objects.

--- app.js
+++ app.js
@@ -12,6 +12,23 @@ myApp.controller('mainController', ['$scope', '$filter', '$timeout', function ($
         return $filter('lowercase')($scope.handle);
     };
 
-
     $scope.characters = 5;
+
+    $scope.rules = [
+
+        {
+            ruleName: "Must be 5 characters"
+        },
+        {
+            ruleName: "Must not be used elsewhere"
+        },
+        {
+            ruleName: "Must be cool"
+        },
+
+    ];
+
+
+    console.log($scope.rules);
+
 }]);
