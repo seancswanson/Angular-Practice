580a8ed859c9ffaaed49c53b48222ada478ed133
seanthaswan <seancswan@gmail.com>
2019-10-28 11:59:10 -0700

    Troubleshooting Routes, introducing $routeParameters to pass values through the URL.

--- app.js
+++ app.js
@@ -5,11 +5,19 @@ var myApp = angular.module('myApp', ['ngRoute']);
 myApp.config(function ($routeProvider) {
 
     $routeProvider
+
         .when('/', {
             templateUrl: 'pages/main.html',
             controller: 'mainController'
         })
-        .when('/ditto', {
+
+        .when('/ditto/', {
+            templateUrl: 'pages/ditto.html',
+            controller: 'dittoController'
+        })
+
+        // Route parameters get passed to scope with $routeParams.[:value]
+        .when('/ditto/:num', {
             templateUrl: 'pages/ditto.html',
             controller: 'dittoController'
         });
@@ -18,6 +26,8 @@ myApp.config(function ($routeProvider) {
 
 // Controller for view inside of myApp
 myApp.controller('mainController', ['$scope', '$filter', '$timeout', '$http', function ($scope, $filter, $timeout, $http) {
+
+    $scope.pageName = 'main';
 
     $scope.handle = '';
 
@@ -44,31 +54,43 @@ myApp.controller('mainController', ['$scope', '$filter', '$timeout', '$http', fu
     $scope.newRule = '';
 
     $scope.addRule = () => {
+
         let newRuleItem = {
             ruleName: $scope.newRule
         };
+
         $scope.rules.push(newRuleItem);
+
         $scope.newRule = '';
 
         console.log($scope.rules);
+
     };
 
     $scope.handleAddRuleClick = () => {
+
         console.log('Rule add:' + $scope.newRule);
 
         $scope.addRule();
+
     }
 
 }]);
 
-myApp.controller('dittoController', ['$scope', '$http', function ($scope, $http) {
-    console.log($scope);
-    $scope.alertClick = () => {
+myApp.controller('dittoController', ['$scope', '$http', '$routeParams', function ($scope, $http, $routeParams) {
+    //    console.log($scope);
+
+    $scope.pageName = 'ditto';
+
+    $scope.num = $routeParams.num || 1;
+
+    $scope.getDitto = () => {
         $scope.name = 'Sean Swanson';
         //        alert('Clicked!');
         $http.get('https://pokeapi.co/api/v2/pokemon/ditto/').then(function (result) {
 
             $scope.dittoInfo = result.data;
+
             $scope.dittoSpriteURL = result.data.sprites.front_default;
 
         }).catch(function (err) {
